40b9b877ba877cf3e4cc42e168963c9947b6da60
ajayjikadra <ajayjikadra001100@gmail.com>
2022-12-17 18:09:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Alret from './Alret.js';
 import './App.css';
 
-function App() {
+import Navbar from './components/Navbar.js';
+import TextForm from './components/TextForm.js';
+
+
+function App(props) {
+
+  const [alret, setAlret] = useState(null);
+
+  const showAlret = (message, type) =>{
+    setAlret({
+      msg: message,
+      type: type
+    })
+    setTimeout(() =>{
+      setAlret(null);   
+    },1500);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+    <Navbar title='TexTU' mode= 'dark'/>
+    <Alret alret={alret}/>
+    <TextForm showAlret={showAlret}/>
+    
+    </>
   );
 }
 
